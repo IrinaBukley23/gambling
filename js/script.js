c983a37bfef48efae8f168243ba9bf4916f598af
Irina <irenserge@mail.ru>
2023-09-04 16:23:20 +0300

    feat: add header adaptive

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log('start project');
+let burger = document.querySelector('.burger');
+let menu = document.querySelector('.header__menu_list');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    menu.classList.toggle('active');
+})
