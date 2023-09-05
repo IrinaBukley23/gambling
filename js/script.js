b82e05a7ba59560f254a23198d86257a352a3b14
Irina <irenserge@mail.ru>
2023-09-05 16:44:04 +0300

    feat: add animation

--- js/script.js
+++ js/script.js
@@ -28,3 +28,43 @@ lantItems.forEach(item => {
         langBtn.innerHTML = `<img src="./img/icons/Union.png" alt="planet"> <span>${e.currentTarget.dataset.lang}</span>` ;
     })
 })
+
+// blog animation
+ 
+document.addEventListener('DOMContentLoaded', function() {
+    let animItems = document.querySelectorAll('.anim-item');
+    const windowAnim = document.querySelector('.main__blog_block');
+    if(animItems.length > 0) {
+        windowAnim.addEventListener('scroll', animOnScroll);
+        function animOnScroll() {
+            for(let i = 0; i < animItems.length; i++) {
+                const animItem = animItems[i];
+                const animItemHeight = animItem.offsetHeight - 50;
+                const animItemOffset = offset(animItem).top;
+                const animStart = 4;
+                
+
+                let animItemPoint = windowAnim.clientHeight - animItemHeight / animStart;
+                if(animItemHeight > windowAnim.clientHeight) {
+                    animItemPoint = windowAnim.clientHeight - windowAnim.clientHeight / animStart;
+                }
+                if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                    animItem.classList.add('_anime');
+                } else {
+                    animItem.classList.remove('_anime');
+
+                }
+            }
+        }
+        function offset(el) {
+            const rect = el.getBoundingClientRect(),
+            scrollLeft = windowAnim.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = windowAnim.pageYOffset || document.documentElement.scrollTop;
+
+            return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+        }
+        animOnScroll();
+    }
+
+    
+});
